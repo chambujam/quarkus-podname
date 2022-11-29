9d63bd805f58530e4e74fe3760558c084d358855
chambujam <104881419+chambujam@users.noreply.github.com>
2022-11-28 22:39:00 -0700

    cameroon

--- src/main/java/org/cytan/PodNameResource.java
+++ src/main/java/org/cytan/PodNameResource.java
@@ -17,7 +17,7 @@ public class PodNameResource {
     @Path("/")
     @Produces(MediaType.TEXT_PLAIN)
     public String getPodName() throws UnknownHostException{
-        return "Welcome Akomnyam! " + InetAddress.getLocalHost().getHostName();
+        return "Welcome to Cameroon! " + InetAddress.getLocalHost().getHostName();
     }
 
 
